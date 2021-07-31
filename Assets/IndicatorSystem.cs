ad939addaf63805e5c55164de2602845e9db3b26
Elias <elias.wt@hotmail.com>
2021-07-31 15:28:15 +0200

    Finished indicator system

--- Assets/IndicatorSystem.cs
+++ Assets/IndicatorSystem.cs
@@ -1,13 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[Serializable]
+public class Indicator
+{
+    public Ingredients type;
+    public Image image;
+    public Transform target;
+    
+    [HideInInspector]
+    public List<Ingredient> ingredientsInWorld;
+
+    [HideInInspector]
+    public PlayerController playerRef;
+
+    public void Update()
+    {
+        if (ingredientsInWorld.Count > 0)
+        {
+            target = GetClosestIngredient();
+
+            if (target != null)
+            {
+                Vector3 targetToViewportPoint = Camera.main.WorldToViewportPoint(target.position);
+
+                targetToViewportPoint.x = Mathf.Clamp01(targetToViewportPoint.x);
+                targetToViewportPoint.y = Mathf.Clamp01(targetToViewportPoint.y);
+            
+                Vector3 targetToScreenPoint = Camera.main.ViewportToScreenPoint(targetToViewportPoint);
+
+                image.transform.position = targetToScreenPoint;   
+            }
+        }
+    }
+    
+    private Transform GetClosestIngredient()
+    {
+        if (playerRef == null) 
+            return null;
+
+        Ingredient closestIngredient = ingredientsInWorld[0];
+        float closestDist = Mathf.Infinity;
+        
+        foreach (Ingredient ingredient in ingredientsInWorld)
+        {
+            float distToPlayer = (ingredient.transform.position - playerRef.transform.position).magnitude;
+            
+            if (distToPlayer <= closestDist)
+            {
+                closestDist = distToPlayer;
+                closestIngredient = ingredient;
+            }
+        }
+
+        return closestIngredient.transform;
+    }
+}
+
 public class IndicatorSystem : MonoBehaviour
 {
-    public Image breadImage;
-    public Image potatoImage;
-    public Image gunPowderImage;
-    
-    
+    public List<Indicator> indicators = new List<Indicator>();
+
+    private IngredientSpawner ingredientSpawner;
+
+    private PlayerController playerRef;
+
+    private void Awake()
+    {
+        ingredientSpawner = FindObjectOfType<IngredientSpawner>();
+        playerRef = FindObjectOfType<PlayerController>();
+        
+        ingredientSpawner.OnIngredientsSpawned += InitIndicators;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        foreach (Indicator indicator in indicators)
+        {
+            indicator.Update();
+        }
+    }
+
+    private void InitIndicators()
+    {
+        foreach (Indicator indicator in indicators)
+        {
+            indicator.ingredientsInWorld = ingredientSpawner.GetAvailableIngredientsOfType(indicator.type);
+            indicator.playerRef = playerRef;
+        }
+    }
 }
